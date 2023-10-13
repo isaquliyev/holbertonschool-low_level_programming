b4ec39d48b27a634192ed97a8d007fc921da65c4
root <isa.quluyev.wip@gmail.com>
2023-10-13 07:21:53 -0700

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-	char a[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	unsigned i = 0;
 
-	for (int i = 0 ; i <= strlen(a) ; i++)
+	for (i = 0 ; i <= strlen(a) ; i++)
 	{
 		putchar (a[i]);
 	}
