3693ae815a29000e9e0914b5e5b078423b6e432e
root <isa.quluyev.wip@gmail.com>
2023-10-13 07:17:29 -0700

    101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	for (int i = 0 ; i <= strlen(a) ; i++)
+	{
+		putchar (a[i]);
+	}
+	return (1);
+}
