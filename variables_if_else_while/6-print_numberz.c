815bee1ce06fc0e42482286175b6dfded367c437
root <isa.quluyev.wip@gmail.com>
2023-10-16 06:19:19 -0700

    5-6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	char c;
+
+	while (a <= 57)
+	{
+		c = a;
+
+		putchar(c);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
