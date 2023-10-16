928934e91e7aff43b0407c7377fb2d43f11e56e6
root <isa.quluyev.wip@gmail.com>
2023-10-16 07:32:42 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
