73f2769cf2a57034f8ee9f815f65bb69bf9ae1f4
root <isa.quluyev.wip@gmail.com>
2023-10-17 07:36:03 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - check lowercase.
+ *
+ * @n: variable.
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -1 * n;
+	}
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
