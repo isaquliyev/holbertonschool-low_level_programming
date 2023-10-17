c55f862568795d9fc8d2b49ea661e2488a5acd38
root <isa.quluyev.wip@gmail.com>
2023-10-17 09:17:27 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - check lowercase.
+ */
+void times_table(void)
+{
+	int i = 0, j = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			if (i * j < 9)
+			{
+				_putchar((i * j) % 10 + '0');
+			}
+			else
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+			_putchar(' ');
+			j++;
+		}
+		j = 0;
+		i++;
+		_putchar('\n');
+	}
+}
