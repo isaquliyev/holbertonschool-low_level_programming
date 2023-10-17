d49b7f077e17596edb7369eca4b144fc099832df
root <isa.quluyev.wip@gmail.com>
2023-10-17 05:01:59 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - prints every character of alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	int i = 0, j = 0;
+
+	while (j < 10)
+	{
+		while (a[i] != '\0')
+		{
+			_putchar(a[i]);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+}
