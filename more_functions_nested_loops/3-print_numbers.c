2f74741119457d6d2dad4667a2ad574e5c4d0fa7
root <isa.quluyev.wip@gmail.com>
2023-10-18 14:21:10 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - that print numbers
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		_putchar(48 + i);
+	}
+	_putchar('\n');
+}
