5da19307a91d2e03338ba46bf633dea95a728300
root <isa.quluyev.wip@gmail.com>
2023-10-19 10:09:52 -0700

    101

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - prints numbers
+ *
+ * @n: integer
+ */
+void print_number(int n)
+{
+	int a = 0;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	while (n != 0)
+	{
+		a = a * 10 + n % 10;
+		n = n / 10;
+	}
+	do {
+		_putchar(a % 10 + '0');
+		a = a / 10;
+	} while (a != 0);
+}
