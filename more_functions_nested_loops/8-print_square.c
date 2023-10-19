4a019c6d0758fbedec9c660db6194e27c4621ed1
root <isa.quluyev.wip@gmail.com>
2023-10-19 08:24:17 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - ...
+ *
+ * @size: variable
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
