3cc742dbcf2bb7495b58996ca7700a43efbfad6b
root <isa.quluyev.wip@gmail.com>
2023-10-20 07:33:08 -0700

    amina qoyduq

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,28 +6,33 @@
  */
 void print_number(int n)
 {
+    unsigned int yeni;
 	int a = 1;
-
 	if (n < 0)
 	{
-		n = n * -1;
-		_putchar('-');
+		yeni = n * -1;
+		putchar('-');
 	}
+	else
+	{
+	    yeni = n;
+	}
+	
 	do {
-		if (n < 10)
+		if (yeni < 10)
 		{
-			_putchar(n + '0');
+			putchar(yeni + '0');
 			break;
 		}
-		a = a * 10 + n % 10;
-		n = n / 10;
-	} while (n != 0);
+		a = a * 10 + yeni % 10;
+		yeni = yeni / 10;
+	} while (yeni != 0);
 	do {
-		if (a < 10)
-		{
-			break;
-		}
-		_putchar(a % 10 + '0');
+	    if (a < 10)
+	    {
+	        break;
+	    }
+		putchar(a % 10 + '0');
 		a = a / 10;
 	} while (a != 0);
 }
