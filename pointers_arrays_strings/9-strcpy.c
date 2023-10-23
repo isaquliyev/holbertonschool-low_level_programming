1372880c892c3b7ad5b52163a9c9078b313f689a
root <isa.quluyev.wip@gmail.com>
2023-10-23 08:24:03 -0700

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,19 @@
 #include <string.h>
 /**
  * _strcpy - copy the string
+ * @dest: destination
+ * @src: source
+ * Return: destination
  */
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int a;
+
+	a = strlen(src);
+	while (a > -1)
+	{
+		*(dest + a) = *(src + a);
+		a--;
+	}
 	return (dest);
 }
