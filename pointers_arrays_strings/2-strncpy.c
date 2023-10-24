7fc27af6c12330ae8d390f4d9bbd9c7a4d070c3e
root <isa.quluyev.wip@gmail.com>
2023-10-24 08:52:05 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copy the str
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ * Return: char array
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, size = strlen(src);
+
+	if (n > size)
+		size = n;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
