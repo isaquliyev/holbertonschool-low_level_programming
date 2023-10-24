0631b7ad1d13381ca7a54c89cf1f0282829049a7
root <isa.quluyev.wip@gmail.com>
2023-10-24 05:24:41 -0700

    101

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,12 +3,28 @@
 #include <time.h>
 /**
  * main - entry point
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+        char s[15];
+        char *c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ0123456789";
+        int n, i = 0, valid = 1, size = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	return (n);
+        srand(time(0));
+        while (i != 8)
+        {
+                n = rand() - RAND_MAX / 2;
+                if (n < 0){
+                    n = n + 1;
+                    n = -1 * n;
+                }
+                *(s + i) = *(c + (n % 62));
+                i++;
+        }
+        *(s + 8) = '1';
+        *(s + 9) = 'a';
+        *(s + 10) = '\0';
+        puts(s);
+        return (0);
 }
