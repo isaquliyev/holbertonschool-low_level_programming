42dcb5dbe569eca94d92c87242d97fa3f1133156
root <isa.quluyev.wip@gmail.com>
2023-10-24 06:39:10 -0700

    101

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,22 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * checksum - executes checksum
+ * @s: input char
+ * Return: checksum
+ */
+int checksum(char *s)
+{
+int sum = 0;
+while (*s != 0)
+{
+	sum += *s;
+	s++;
+}
+return (sum);
+}
 /**
  * main - entry point
  * Return: always 0
@@ -9,14 +25,23 @@ int main(void)
 {
         char s[33];
         char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
-        int i = 0;
+        int n, i = 0, valid = 0;
+        unsigned int sum;
         srand(time(NULL));
+        while (valid == 0){
         while (i != 33)
         {
                 *(s + i) = *(c + (rand() % (sizeof(c) - 1)));
                 i++;
         }
         *(s + 33) = '\0';
-        printf("%s",s);
+        sum = checksum(s);
+		if (sum == 2772)
+		{
+			valid = 1;
+			printf("%s", s);
+		}
+		i = 0;
+        }
         return(0);
 }
