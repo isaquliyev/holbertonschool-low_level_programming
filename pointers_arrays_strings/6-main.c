db79e0bdbbfb3bb1e90760f905b79d9fd534684b
root <isa.quluyev.wip@gmail.com>
2023-10-24 15:13:59 -0700

    capitalize

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -1,15 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-    char *str;
+	    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	        char *ptr;
 
-    str = "0123456789";
-    puts2(str);
-    return (0);
+		    ptr = cap_string(str);
+		        printf("%s", ptr);
+			    printf("%s", str);
+			        return (0);
 }
