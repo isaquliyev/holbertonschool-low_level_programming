a172da4ce44368911b2dbde99740215b8c4d1b7f
root <isa.quluyev.wip@gmail.com>
2023-10-27 09:23:36 -0700

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_chessboard - prints chess pieces location
+ * @a: two dimensional array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf("%c", a[i][j]);
+		printf("\n");
+	}
+}
