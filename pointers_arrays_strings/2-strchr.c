db5fb5edce7312421aec99b9c182892130764a11
root <isa.quluyev.wip@gmail.com>
2023-10-27 07:52:00 -0700

    3.2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
-/** _strchr - function
+/**
+ * _strchr - function
  * @s: source
  * @c: special character
  * Return: modified string
  */
 char *_strchr(char *s, char c)
 {
-	while (*(s) != c)
+	while (*(s) != c || *(s) != '\0')
 		s++;
 	return (s);
 }
