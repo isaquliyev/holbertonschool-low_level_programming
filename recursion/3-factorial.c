358e6b421c78b69888f40acbd9c0bed62fc13d7f
root <isa.quluyev.wip@gmail.com>
2023-10-29 14:09:40 -0700

    3-factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,13 @@
+/**
+ * factorial - finds the factorial of number
+ * @n: given number
+ * Return: factorial of number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
