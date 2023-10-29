3d120246be1919ff1d7920f76c44c43383530bf5
root <isa.quluyev.wip@gmail.com>
2023-10-29 14:03:02 -0700

    2-strlen_recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+/**
+ * _strlen_recursion - finds length of string
+ * @s: given string
+ * Return: integer which is equal to length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return 0;
+	return (1 + _strlen_recursion(s + 1));
+}
