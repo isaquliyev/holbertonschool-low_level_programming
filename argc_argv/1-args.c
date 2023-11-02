b377e8e3e9aa3e3fe167184a98f0bb91a9fdfaec
root <isa.quluyev.wip@gmail.com>
2023-11-02 05:30:24 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - main point
+ * @argc: number of argv
+ * @argv: vector
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
