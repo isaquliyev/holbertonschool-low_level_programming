6b4cf2a4dd31be08d1e92bc81cf80e261aeee68f
root <isa.quluyev.wip@gmail.com>
2023-11-02 06:13:47 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main point
  * @argc: count
@@ -8,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	if (argc > 2)
-		printf("%d\n", (argv[1][0] - 48) * (argv[2][0] - 48));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	else
 		printf("Error\n");
 	return (0);
