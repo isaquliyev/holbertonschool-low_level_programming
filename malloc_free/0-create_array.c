157563cdcdd9884566d13580275b6ab47576e468
root <isa.quluyev.wip@gmail.com>
2023-11-06 04:13:44 -0800

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	while (i < size)
