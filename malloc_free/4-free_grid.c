2f332eee21ddf3ace3e47c86f1c224417e2756d7
root <isa.quluyev.wip@gmail.com>
2023-11-06 06:53:29 -0800

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * free_grid - make grid freeee
+ * @grid: grid
+ * @height: height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+	for (i = height; i >= 0; height--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
