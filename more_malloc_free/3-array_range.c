8fef0ba4f661b20c302119804f3b3959bec1ff80
root <isa.quluyev.wip@gmail.com>
2023-11-09 10:59:16 -0800

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,7 +11,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc((max - min) * sizeof(int) + 1);
+	p = malloc((max - min + 1) * sizeof(int));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < max - min + 1; i++)
---
+++ more_malloc_free/3-main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *  * simple_print_buffer - prints buffer in hexa
+ *   * @buffer: the address of memory to print
+ *    * @size: the size of the memory to print
+ *     *
+ *      * Return: Nothing.
+ *       */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+		unsigned int i;
+
+			i = 0;
+				while (i < size)
+						{
+									if (i % 10)
+												{
+																printf(" ");
+																		}
+											if (!(i % 10) && i)
+														{
+																		printf("\n");
+																				}
+													printf("0x%02x", buffer[i]);
+															i++;
+																}
+					printf("\n");
+}
+
+/**
+ *  * main - check the code .
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		int *a;
+
+			a = array_range(1024, 2048);
+				if (a == NULL)
+						{
+									printf("Failed\n");
+											return (1);
+												}
+					simple_print_buffer(a, 1024);
+						free(a);
+							return (0);
+}
