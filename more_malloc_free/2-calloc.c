d1414a0aee3627626bc88093f41867e18e5e2d4d
root <isa.quluyev.wip@gmail.com>
2023-11-09 09:55:58 -0800

    2-calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,10 +3,20 @@
  * _calloc - function
  * @nmemb: number of elements
  * @size: size of single byte
+ * Return: memory allocated pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int *p, i = 0;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	return (malloc(nmemb * size));
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
 }
