f369007510adee6a9b080d58545c9048a19ca5aa
root <isa.quluyev.wip@gmail.com>
2023-11-17 06:21:00 -0800

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,25 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
-
 /**
- * consist_number - checks given string consist of numbers between 1 and 9
- * @number: given number as char array
- * Return: 0 if there is any different character, otherwise 1
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int consist_number(char *number)
+int is_digit(char *s)
 {
 	int i = 0;
 
-	while (*(number + i) != '\0')
+	while (s[i])
 	{
-		if (*(number + i) < 48 || *(number + i) > 57)
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
 	}
 	return (1);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -31,25 +54,22 @@ int main(int argc, char *argv[])
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = *(argv + 1), s2 = *(argv + 2);
-	if (argc != 3 || consist_number(s1) == 0 || consist_number(s2) == 0)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-	if (result == NULL)
+	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-		*(result + i) = 0;
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
