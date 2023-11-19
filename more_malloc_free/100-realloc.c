4cf493b13751ca70dc46416f94dffda64707626e
root <isa.quluyev.wip@gmail.com>
2023-11-19 08:51:38 -0800

    100

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+/**
+ *  * _realloc - realloc.
+ *   * @ptr: old pointer.
+ *    * @old_size: old size.
+ *     * @new_size: new size.
+ *      * Return: new pointer.
+ *       */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+		int i;
+			void *new_ptr;
+
+				if (new_size == old_size)
+							return (ptr);
+					if (ptr == NULL)
+							{
+										free(ptr);
+												new_ptr = malloc(new_size);
+														return (new_ptr);
+															}
+						if (new_size == 0)
+								{
+											free(ptr);
+													return (NULL);
+														}
+							new_ptr = malloc(new_size);
+								if (new_ptr == NULL)
+										{
+													free(ptr);
+															return (NULL);
+																}
+									for (i = 0; i < (int)new_size; i++)
+											{
+														if (i >= (int)old_size)
+																		break;
+																*((char *)new_ptr + i) = *((char *)ptr + i);
+																	}
+										free(ptr);
+											return (new_ptr);
+}
