d9d8f0c7984bc016b049d316e25276872fd525b3
root <isa.quluyev.wip@gmail.com>
2023-11-19 15:26:56 -0800

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog data structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: dog attributes
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
