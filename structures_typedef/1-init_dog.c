f927fd37554bccfe4029679845189fb41db31e3c
root <isa.quluyev.wip@gmail.com>
2023-11-19 15:32:48 -0800

    dog_init

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * init_dog - initialize valuse to dog
+ * @d: dog structure
+ * @name: name of dog
+ * @age: age of th dog
+ * @owner: owner of the dog
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
