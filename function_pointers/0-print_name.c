0259ce56a90c242bcb1794ace9e2d302610deed9
root <isa.quluyev.wip@gmail.com>
2023-11-20 02:04:16 -0800

    print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
 /**
  * print_name - prints name using given function
  * @name: name
  * @f: function which prints string
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	(*f)(name);
 }
