d904befe01a0df6966958997d8074eb5cab6253f
root <isa.quluyev.wip@gmail.com>
2023-11-20 02:55:48 -0800

    int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,16 +4,17 @@
  * @array: given array
  * @size: size of array
  * @cmp: function pointer performs compare functions
+ * Return: return index of first occurance
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, index = -1;
 
-	if (size <= 0)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if((*cmp)(*(array + i)))
+		if ((*cmp)(*(array + i)))
 		{
 			index = i;
 			break;
