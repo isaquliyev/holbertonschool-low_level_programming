62f362a969f470a06d841e1e0d5bbda3d2598ed7
root <isa.quluyev.wip@gmail.com>
2023-11-21 04:40:00 -0800

    print_numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,10 +1,22 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * print_numbers - prints numbers which written on prompt
+ * @separator: separator between 2 sequental elements
+ * @n: number of integers will pass
+ * Return: nothing
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i = 0;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(ptr, n);
 	for (i = 0; i < n - 1; i++)
 	{
@@ -12,5 +24,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator)
 			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(ptr, int));
+	printf("%d", va_arg(ptr, int));
+	printf("\n");
 }
