56032117298e83d8c6aa0f52d18c6e73b35d9050
root <isa.quluyev.wip@gmail.com>
2023-11-21 04:13:25 -0800

    sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0, i;
+	va_list ptr;
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+	va_end(ptr);
+	return (sum);
+}
