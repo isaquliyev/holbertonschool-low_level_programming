6381a20c0e60d9ba1c35b4d6f270a4c6abae6d95
root <isa.quluyev.wip@gmail.com>
2023-11-21 04:16:25 -0800

    betty issues solved

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,10 +1,15 @@
 #include <stdarg.h>
+/**
+ * sum_them_all - addition of all arguments
+ * @n: number of given integers
+ * Return: sum of these arguments
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0, i;
 	va_list ptr;
-	va_start(ptr, n);
 
+	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 	va_end(ptr);
