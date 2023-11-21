801774db74611af831cddc59336c83857aa8cc8b
root <isa.quluyev.wip@gmail.com>
2023-11-21 05:45:53 -0800

    print_strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints strings given in prompt
+ * @separator: separator between string
+ * @n: number of strings
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ptr, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%s", va_arg(ptr, char *));
+		if (separator)
+			printf("%s", separator);
+	}
+	printf("%s", va_arg(ptr, char *));
+	printf("\n");
+}
