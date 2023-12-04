9bad49d2d43465ed7c42ad51860e6c72e8f1c522
root <isa.quluyev.wip@gmail.com>
2023-12-04 08:06:51 -0800

    NULL case fixed

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,8 +14,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	while (index)
+	{
 		head = head->next, index--;
-	if (!head)
-		return (NULL);
+		if (head == NULL)
+			return (NULL);
+	}
 	return (head);
 }
