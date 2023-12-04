c9179e9f341f48f932a63c07c7553bb559deca70
root <isa.quluyev.wip@gmail.com>
2023-12-04 04:17:30 -0800

    betty style fixed

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-void tartoise (void) __attribute__ ((constructor));
+void tartoise(void) __attribute__ ((constructor));
 
 void tartoise(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
