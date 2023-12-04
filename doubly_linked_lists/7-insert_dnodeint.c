351a7684b4ac651e88fc39da0818690d7d94697b
root <isa.quluyev.wip@gmail.com>
2023-12-04 13:57:39 -0800

    insert at given index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserting new node at given position.
+ *
+ * @h: given double linked list.
+ *
+ * @idx: particular index that will contain next node.
+ *
+ * @n: the value of new node.
+ *
+ * Return: modified double linked list.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *first_part = *h;
+	unsigned int i = 0;
+
+	if (!new_node)
+		return (NULL);
+	while (i < idx - 1)
+	{
+		i++, first_part = first_part->next;
+		if (!first_part)
+			return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = first_part;
+	new_node->next = first_part->next;
+	first_part->next = new_node;
+	return (*h);
+}
