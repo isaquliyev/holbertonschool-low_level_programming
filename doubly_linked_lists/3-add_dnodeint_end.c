dbf3215f06e09681af8e931c50b3e6e0432c35ad
root <isa.quluyev.wip@gmail.com>
2023-12-04 06:36:43 -0800

    add new node at the end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds new node at the end.
+ * 
+ * @head: initial linked list.
+ *
+ * @n: the integer value of new node.
+ *
+ * Return: modified linked list.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *last_node = *head;
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
+	return (*head);
+}
