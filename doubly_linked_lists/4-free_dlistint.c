ea0ad47fe170e1b54fe11ab2086441de033f44b1
root <isa.quluyev.wip@gmail.com>
2023-12-04 06:54:09 -0800

    4_free_dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
