53206cf371a43bf74f9b90cc904cbe56a90d7f3f
root <isa.quluyev.wip@gmail.com>
2023-12-04 06:55:58 -0800

    betty style provided

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - frees the double linked list
+ *
+ * @head: initial linked list
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
+
 	while (head)
 	{
 		temp = head;
