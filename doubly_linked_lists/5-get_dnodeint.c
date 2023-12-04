f28c205cc84e2631eb8ff2b5c77a9dfd22e2f265
root <isa.quluyev.wip@gmail.com>
2023-12-04 07:49:19 -0800

    nth node of a dlistint_t linked list

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that takes particular node given index.
+ *
+ * @head: given linked list.
+ *
+ * @index: index of node.
+ *
+ * Return: particular node situated in given index.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	while (index)
+		head = head->next, index--;
+	return (head);
+}
