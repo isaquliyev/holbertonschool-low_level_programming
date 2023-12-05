427bb6db5f5d7a7fb41f93a328540464f2718886
root <isa.quluyev.wip@gmail.com>
2023-12-05 00:52:10 -0800

    idx == 0 condition added

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,15 +21,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!new_node)
 		return (NULL);
-	while (i < idx - 1)
+	if (idx == 0)
 	{
-		i++, first_part = first_part->next;
-		if (!first_part)
-			return (NULL);
+		new_node->n = n;
+		new_node->next = *h;
+		new_node->prev = NULL;
+		*h = new_node;
 	}
-	new_node->n = n;
-	new_node->prev = first_part;
-	new_node->next = first_part->next;
-	first_part->next = new_node;
+	else
+	{
+		while (i < idx - 1)
+		{
+			i++, first_part = first_part->next;
+			if (!first_part)
+				return (NULL);
+		}
+		new_node->n = n;
+		new_node->prev = first_part;
+		new_node->next = first_part->next;
+		first_part->next = new_node;
+	}
 	return (*h);
 }
