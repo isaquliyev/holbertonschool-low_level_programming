db3efb7ab784dfb706e6c327a7428e8058e819ad
root <isa.quluyev.wip@gmail.com>
2023-12-06 04:02:13 -0800

    7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,31 +17,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *first_part = *h;
-	unsigned int i = 0;
+	unsigned i = 0;
 
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	if (idx == 0)
+	if (new_node)
 	{
-		if ((*h))
-			new_node->next = *h, (*h)->prev = new_node;
-		new_node->prev = NULL;
-		*h = new_node;
-		return (*h);
-	}
-	else
-	{
-		while (i < idx - 1)
+		new_node->n = n;
+		if (idx == 0)
 		{
-			if (!first_part)
-				return (NULL);
-			i++, first_part = first_part->next;
+			if (*h)
+			{
+				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
+			*h = new_node;
+			return (new_node);
 		}
-		new_node->prev = first_part;
-		new_node->next = first_part->next;
-		first_part->next = new_node;
-		return (*h);
+		while (i < idx - 1 && first_part)
+		{
+			first_part = first_part->next;
+			i++;
+		}
+		if (first_part)
+		{
+			if (first_part->next)
+				first_part->next->prev = new_node;
+			new_node->next = first_part->next;
+			new_node->prev = first_part;
+			first_part->next = new_node;
+			return (new_node);
+		}
 	}
 	return (NULL);
 }
