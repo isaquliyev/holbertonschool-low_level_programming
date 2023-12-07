76de22808cc02e10eb9f2196fd58e6f54a587c55
root <isa.quluyev.wip@gmail.com>
2023-12-07 15:31:23 -0800

    print decimal as binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	int last = n % 2;
+
+	if (!n)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+	else
+	{
+		_putchar('1'), n /= 2;
+		while (n > 1)
+			_putchar(n % 2 + '0'), n /= 2;
+		_putchar(last + '0');
+	}
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,4 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+int _putchar(char c);
+
 unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
 #endif
