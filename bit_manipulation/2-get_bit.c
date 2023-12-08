1ee9448da9e7878b3c57d0c3d876972b5e4e1e16
root <isa.quluyev.wip@gmail.com>
2023-12-08 03:27:49 -0800

    get_bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+/**
+ * get_bit - get value of bit at given index.
+ *
+ * @n: given integer.
+ *
+ * @index: which index we need.
+ *
+ * Return: a bit which we want to find.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index)
+		index--, n = n >> 1;
+	n = n & 1;
+	return (n);
+}
