5c80e74efbf8eb3941367becef116ae8437ee03d
root <isa.quluyev.wip@gmail.com>
2023-12-11 08:58:45 -0800

    O_APPEND usage

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -7,5 +7,15 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, w;
 
+	if (!filename)
+		return(-1);
+	if (!text_content)
+		text_content = "";
+	fd = open(filename, O_RDWR | O_APPEND);
+	w = write(fd, text_content, strlen(text_content));
+	if (fd == -1 || w == -1)
+		return (-1);
+	return (1);
 }
