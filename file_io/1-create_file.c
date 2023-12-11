69471114c2d175fb7464cbfc6d1c98b3649dec7f
root <isa.quluyev.wip@gmail.com>
2023-12-11 05:56:02 -0800

    I hope that NULL case fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, i = 0i, len = strlen(text_content);
+	int fd, w, i = 0, len = strlen(text_content);
 	char *buf;
 
 	buf = malloc(strlen(text_content) + 1);
