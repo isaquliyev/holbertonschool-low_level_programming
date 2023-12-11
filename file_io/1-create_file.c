079aeba7886bb829755244d5549d491118eede19
root <isa.quluyev.wip@gmail.com>
2023-12-11 05:46:08 -0800

    NULL case fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,6 +29,7 @@ int create_file(const char *filename, char *text_content)
 		*(buf + i) = *(text_content + i);
 		i++;
 	}
+	*(buf + i) = '\0';
 	fd = open(filename, O_EXCL);
 	if (fd == 3)
 		fd = open(filename, O_TRUNC | O_WRONLY);
