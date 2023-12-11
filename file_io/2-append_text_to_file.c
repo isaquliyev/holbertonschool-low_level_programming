cbc240a8cd5d15f66a4cb54346b46f67171851a6
root <isa.quluyev.wip@gmail.com>
2023-12-11 09:01:53 -0800

    O_APPEND usage

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,12 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * append_text_to_file - adds text_content end of filename.
+ *
+ * @filename: name of file.
+ *
+ * @text_content: text will add at the end of filename.
+ *
+ * Return: 1 if success, otherwise 0.
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w;
 
 	if (!filename)
-		return(-1);
+		return (-1);
 	if (!text_content)
 		text_content = "";
 	fd = open(filename, O_RDWR | O_APPEND);
