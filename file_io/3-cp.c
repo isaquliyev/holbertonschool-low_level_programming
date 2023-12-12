1f202bd29d65c29bc78c004b6c9703c4c2e40594
root <isa.quluyev.wip@gmail.com>
2023-12-12 05:51:49 -0800

    cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#define BUFFER_SIZE 1024
+
+/**
+ * copier - copy the content of first argument to another.
+ *
+ * @file_from: source file.
+ *
+ * @file_to: destination file.
+ */
+
+void copier(const char *file_from, const char *file_to)
+{
+	int fd, r, w, c;
+	char *buf = malloc(BUFFER_SIZE);
+
+	fd = open(file_from, O_RDONLY);
+	r = read(fd, buf, BUFFER_SIZE);
+	if (fd == -1 || r == -1)
+	{
+		dprintf(STRERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	umask(0);
+	fd = open(file_to, O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	w = write(fd, buf, strlen(buf));
+	if (fd == -1 || w == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
+	c = close(fd);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	free(buf);
+}
+
+/**
+ * main - main block.
+ *
+ * @argc: count of arguments.
+ *
+ * @argv: argument vector.
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = argv[1];
+	file_to = argv[2];
+	copier(file_from, file_to);
+	return (0);
+}
