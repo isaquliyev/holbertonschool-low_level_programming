4a9acf3a914d65edfdbf11d879695c2ccc7abba4
root <isa.quluyev.wip@gmail.com>
2023-12-16 08:41:34 -0800

    documentation added

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
 	unsigned long int i = 0;
-	char checker = 0;
+	char f = 0;
 	
 	putchar('{');
 	while (i < ht->size)
@@ -19,11 +19,12 @@ void hash_table_print(const hash_table_t *ht)
 		tmp = ht->array[i];
 		while (tmp)
 		{
-			if (checker == 1)
+			if (f)
 				printf(", ");
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			checker = 1;
 			tmp = tmp->next;
+			if(!f)
+				f++;
 		}
 		i++;
 	}
