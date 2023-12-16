8dad8e92e2163a0e7552e03142651fffee1f9daa
root <isa.quluyev.wip@gmail.com>
2023-12-16 09:11:25 -0800

    deletion of hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,6 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (!strcmp(ht->array[index]->key, key))
 		{
+			free(ht->array[index]->value);
 			ht->array[index]->value = strdup(value);
 			return (1);
 		}
---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes hash table.
+ *
+ * @ht: given hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			temp = ht->array[i];
+			while (temp->next)
+			{
+				free(temp->key);
+				free(temp->value);
+				temp = temp->next;
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
