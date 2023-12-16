39a81c6da740c5c2a19d830051bd83aaa46b1fe8
root <isa.quluyev.wip@gmail.com>
2023-12-16 08:42:53 -0800

    documentation added

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (f)
 				printf(", ");
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", tmp->key, tmp->value);
 			tmp = tmp->next;
 			if(!f)
 				f++;
