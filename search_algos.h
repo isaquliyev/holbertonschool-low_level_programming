8342f139125dc7cc0556e6fd9d360740c9d446b5
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-18 01:17:19 -0800

    search_algos.h header file created.

---
+++ search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* ESSENTIAL LIBRARIES */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+/* FUNCTION PROTOTYPES */
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
